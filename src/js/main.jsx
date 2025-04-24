c3597f9a922d590e6107db9bc23c67574d7b6945
Madeleyner Fernandez <129197262+Madeleyner@users.noreply.github.com>
2025-04-24 01:06:35 +0000

    contadorSimple.MF

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -5,14 +5,20 @@ import ReactDOM from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
 
+
 // index.css'
 import '../styles/index.css'
 
 // components
 import Home from './components/Home';
+import SecondsCounter from './components/SecondsCounter';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+let time = 0
+
+function contador(){
+
+ReactDOM.createRoot(document.getElementById('root')).render(<SecondsCounter seconds={time} />)
+time++
+}
+setInterval(contador, 1000)
+
